83eb041151742a9a82088d77b611147bff3dc186
Anastasiia Kovtun <nastia@Anastasiias-MacBook-Pro.local>
2020-05-21 10:58:08 +0300

    add gitignore and providers.tf

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  required_version = ">= 0.12.18"
+}
+
+provider "google" {
+  version = "~> 3.8"
+  region  = "europe-west-1"
+}
+
+provider "google-beta" {
+  region = "europe-west-1"
+}
